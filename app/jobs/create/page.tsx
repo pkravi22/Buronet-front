17f3e7e2ff76f32542eaebc816a8739fd1b52316
unknown <pramendrasinghravi@gmail.com>
2026-06-30 18:32:21 +0530

    Fix missing useEffect import in CreateJobPage

--- app/jobs/create/page.tsx
+++ app/jobs/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../../../components/Navbar';
 import TopBar from '../../../components/TopBar';
 import { useRouter } from 'next/navigation';
